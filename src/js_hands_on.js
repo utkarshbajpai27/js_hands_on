f4ba611b6fa11c223ca46844a8168b3365fb2108
utkarshbajpai27 <utkarshbajpai@cedcoss.com>
2022-02-05 14:14:12 +0530

    use conditional logic with if statement

--- src/js_hands_on.js
+++ src/js_hands_on.js
@@ -144,6 +144,15 @@ function nextInLine(arr, item) {
   }
 
 
+  function trueOrFalse(wasThatTrue) {
+    if(wasThatTrue){
+      return "Yes, that was true"
+    }
+    return "No, that was false"
+  }
+  trueOrFalse(true);
+  trueOrFalse(false);
+
 
 
 
