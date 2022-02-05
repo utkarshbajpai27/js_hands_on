46bdbe4d21d7fcede5783d94566463294b7dde21
utkarshbajpai27 <utkarshbajpai@cedcoss.com>
2022-02-05 13:29:36 +0530

    global scope and funciton

--- src/js_hands_on.js
+++ src/js_hands_on.js
@@ -80,5 +80,23 @@ function reusableFunction(){
         function timesFive(para){
             return para*5;
     }
+
+    let myGlobal=10; 
+
+function fun1() {
+  oopsGlobal=5;
+}
+
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
 }
  
