e4437e0c9f3be1ebc6c4018fe39fab580966edaa
utkarshbajpai27 <utkarshbajpai@cedcoss.com>
2022-02-05 11:15:06 +0530

    Decimal number

--- src/js_hands_on.js
+++ src/js_hands_on.js
@@ -1,15 +1,18 @@
 /* multiline 
 comment*/
 //single line comment
-<script>
-  var myName; var a; a=7; var b; b=a; var c=9; var myFirstName="hello"; var
-  myLastName="hello"; var d=1; var e=5; var f="I am a"; d = d + 1; e= e + 5; f =
-  f + " String!"; var studlyCapVar; var properCamelCase; var titleCaseOver;
-  studlyCapVar=10; properCamelCase="A String" titleCaseOver=9000; let catName ="Oliver"; let catSound = "Meow!"; const FCC = "freeCodeCamp"; let fact = "is
-  cool!"; fact = "is awesome!"; console.log(FCC, fact); const sum = 10 + 10;
-  const difference = 45 - 33;
-  const product = 8 * 10;
-  const quotient = 66 / 33;
-  let myVar = 87;
-myVar--;
-</script>;
+function myfunction(){
+    var myName; var a; a=7; var b; b=a; var c=9; var myFirstName="hello"; var
+    myLastName="hello"; var d=1; var e=5; var f="I am a"; d = d + 1; e= e + 5; f =
+    f + " String!"; var studlyCapVar; var properCamelCase; var titleCaseOver;
+    studlyCapVar=10; properCamelCase="A String" titleCaseOver=9000; let catName ="Oliver"; let catSound = "Meow!"; const FCC = "freeCodeCamp"; let fact = "is
+    cool!"; fact = "is awesome!"; console.log(FCC, fact); const sum = 10 + 10;
+    const difference = 45 - 33;
+    const product = 8 * 10;
+    const quotient = 66 / 33;
+    let myVar = 87;
+  myVar--;
+  const myDecimal = 5.7;
+
+}
+ 
