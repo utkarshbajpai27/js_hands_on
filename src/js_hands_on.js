c53eee3b6b7c3cffc5b0a974b4312ca658ccddd4
utkarshbajpai27 <utkarshbajpai@cedcoss.com>
2022-02-05 11:17:08 +0530

    Multiply two decimal number

--- src/js_hands_on.js
+++ src/js_hands_on.js
@@ -8,7 +8,7 @@ function myfunction(){
     studlyCapVar=10; properCamelCase="A String" titleCaseOver=9000; let catName ="Oliver"; let catSound = "Meow!"; const FCC = "freeCodeCamp"; let fact = "is
     cool!"; fact = "is awesome!"; console.log(FCC, fact); const sum = 10 + 10;
     const difference = 45 - 33;
-    const product = 8 * 10;
+    const product = 2.5 * 2.0;
     const quotient = 66 / 33;
     let myVar = 87;
   myVar--;
