4b83a2dbb7b7e9feb37caeee34989797584d43ec
utkarshbajpai27 <utkarshbajpai@cedcoss.com>
2022-02-05 11:03:10 +0530

    Subtract one number

--- src/js_hands_on.js
+++ src/js_hands_on.js
@@ -5,7 +5,7 @@ comment*/
   var myName; var a; a=7; var b; b=a; var c=9; var myFirstName="hello"; var
   myLastName="hello"; var d=1; var e=5; var f="I am a"; d = d + 1; e= e + 5; f =
   f + " String!"; var studlyCapVar; var properCamelCase; var titleCaseOver;
-  studlyCapVar=10; properCamelCase="A String" titleCaseOver=9000; let catName =
-  "Oliver"; let catSound = "Meow!"; const FCC = "freeCodeCamp"; let fact = "is
+  studlyCapVar=10; properCamelCase="A String" titleCaseOver=9000; let catName ="Oliver"; let catSound = "Meow!"; const FCC = "freeCodeCamp"; let fact = "is
   cool!"; fact = "is awesome!"; console.log(FCC, fact); const sum = 10 + 10;
+  const difference = 45 - 33;
 </script>;
