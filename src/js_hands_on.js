a3eec5a4a96134fd6f6c8b84bc91371df8b69237
utkarshbajpai27 <utkarshbajpai@cedcoss.com>
2022-02-05 11:00:54 +0530

    Add two numbers

--- src/js_hands_on.js
+++ src/js_hands_on.js
@@ -2,33 +2,10 @@
 comment*/
 //single line comment
 <script>
-var myName;
-var a;
-a=7;
-var b;
-b=a;
-var c=9;
-var myFirstName="hello";
-var myLastName="hello";
-var d=1;
-var e=5;
-var f="I am a";
-d = d + 1;
-e= e + 5;
-f = f + " String!";
-
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-studlyCapVar=10;
-properCamelCase="A String"
-titleCaseOver=9000;
-
-let catName = "Oliver";
-let catSound = "Meow!";
-
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
-console.log(FCC, fact); // Change this line
-</script>
+  var myName; var a; a=7; var b; b=a; var c=9; var myFirstName="hello"; var
+  myLastName="hello"; var d=1; var e=5; var f="I am a"; d = d + 1; e= e + 5; f =
+  f + " String!"; var studlyCapVar; var properCamelCase; var titleCaseOver;
+  studlyCapVar=10; properCamelCase="A String" titleCaseOver=9000; let catName =
+  "Oliver"; let catSound = "Meow!"; const FCC = "freeCodeCamp"; let fact = "is
+  cool!"; fact = "is awesome!"; console.log(FCC, fact); const sum = 10 + 10;
+</script>;
