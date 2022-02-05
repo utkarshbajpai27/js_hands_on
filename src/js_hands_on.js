e0ed364bb4247464441c73d2e1f27fe4183c005b
utkarshbajpai27 <utkarshbajpai@cedcoss.com>
2022-02-05 11:27:08 +0530

    Compond assignment subtraction operator

--- src/js_hands_on.js
+++ src/js_hands_on.js
@@ -3,7 +3,7 @@ comment*/
 //single line comment
 function myfunction(){
     var myName; var a; a=7; var b; b=a; var c=9; var myFirstName="hello"; var
-    myLastName="hello"; var d=1; var e=5; var f="I am a"; d += 1; e+= 5; f =
+    myLastName="hello"; var d=1; var e=5; var f="I am a"; d -= 1; e-= 5; f =
     f + " String!"; var studlyCapVar; var properCamelCase; var titleCaseOver;
     studlyCapVar=10; properCamelCase="A String" titleCaseOver=9000; let catName ="Oliver"; let catSound = "Meow!"; const FCC = "freeCodeCamp"; let fact = "is
     cool!"; fact = "is awesome!"; console.log(FCC, fact); const sum = 10 + 10;
